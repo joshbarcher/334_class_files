fcac5f6b0ce817328e2a05d5399867eaa0584927
Josh Archer <jarcher@greenriver.edu>
2022-04-20 08:13:47 -0700

    Added starter files for a lab on writing the basic sorts.

---
+++ basic_sorts/src/sorts/TestSorts.java
@@ -0,0 +1,24 @@
+package sorts;
+
+public class TestSorts
+{
+    public static void main(String[] args)
+    {
+        //test your sorts here...
+    }
+
+    public void bubbleSort(int[] array)
+    {
+        //do something...
+    }
+
+    public void selectionSort(int[] array)
+    {
+        //do something...
+    }
+
+    public void insertionSort(int[] array)
+    {
+        //do something...
+    }
+}
